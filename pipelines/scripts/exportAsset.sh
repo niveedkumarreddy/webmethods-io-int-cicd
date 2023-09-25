958d435f5a55bf9ebc9781ccbea48147d238f375
insrsa <insrsa@softwareag.com>
2023-09-25 21:02:56 +0200

    export Reference Data

--- pipelines/scripts/exportAsset.sh
+++ pipelines/scripts/exportAsset.sh
@@ -74,8 +74,7 @@ function echod(){
 
 echod $@
 
-function exportAsset(){
-
+function exprtReferenceData (){
 LOCAL_DEV_URL=$1
 admin_user=$2
 admin_password=$3
@@ -84,103 +83,7 @@ assetID=$5
 assetType=$6
 HOME_DIR=$7
 
-echod ${assetType}
-
-if [[ $assetType = workflow* ]]; then
-        echod $assetType
-        FLOW_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}/workflows/${assetID}/export
-        cd ${HOME_DIR}/${repoName}
-        mkdir -p ./assets/workflows
-        cd ./assets/workflows
-        echod "Workflow Export:" ${FLOW_URL}
-        echod $(ls -ltr)
-    else
-        FLOW_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}/flows/${assetID}/export
-        cd ${HOME_DIR}/${repoName}
-        mkdir -p ./assets/flowservices
-        cd ./assets/flowservices
-        echo "Flowservice Export:" ${FLOW_URL}
-        echod $(ls -ltr)
-    fi    
-
-  
-    linkJson=$(curl  --location --request POST ${FLOW_URL} \
-    --header 'Content-Type: application/json' \
-    --header 'Accept: application/json' \
-    -u ${admin_user}:${admin_password})
-
-    downloadURL=$(echo "$linkJson" | jq -r '.output.download_link')
-    
-    regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
-    
-    if [[ $downloadURL =~ $regex ]]; then 
-       echod "Valid Download link retreived:"${downloadURL}
-    else
-        echo "Download link retreival Failed:" ${linkJson}
-        exit 1
-    fi
-    downloadJson=$(curl --location --request GET "${downloadURL}" --output ${assetID}.zip)
-
-    FILE=./${assetID}.zip
-    if [ -f "$FILE" ]; then
-        echo "Download succeeded:" ls -ltr ./${assetID}.zip
-    else
-        echo "Download failed:"${downloadJson}
-    fi
-cd ${HOME_DIR}/${repoName}
-
-}  
-if [ ${synchProject} == true ]; then
-  echod "Listing All Assets"
-  echod $assetType
-  PROJECT_LIST_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}/assets
-
-  projectListJson=$(curl  --location --request GET ${PROJECT_LIST_URL} \
-    --header 'Content-Type: application/json' \
-    --header 'Accept: application/json' \
-    -u ${admin_user}:${admin_password})
-  
-  # Exporing Workflows
-  for item in $(jq  -c -r '.output.workflows[]' <<< "$projectListJson"); do
-    echod "Inside Workflow Loop"
-    assetID=$item
-    assetType=workflow
-    echod $assetID
-    exportAsset ${LOCAL_DEV_URL} ${admin_user} ${admin_password} ${repoName} ${assetID} ${assetType} ${HOME_DIR}
-  done
-  # Exporting Flows
-  for item in $(jq  -c -r '.output.flows[]' <<< "$projectListJson"); do
-    echod "Inside FS Loop"
-    assetID=$item
-    assetType=flowservice
-    echod $assetID
-    exportAsset ${LOCAL_DEV_URL} ${admin_user} ${admin_password} ${repoName} ${assetID} ${assetType} ${HOME_DIR}
-  done
-
-  #Expoting Accounts
-  ACCOUNT_LIST_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}/accounts
-
-  accountListJson=$(curl  --location --request GET ${ACCOUNT_LIST_URL} \
-      --header 'Content-Type: application/json' \
-      --header 'Accept: application/json' \
-      -u ${admin_user}:${admin_password})
-
-
-      accountexport=$(echo "$accountListJson" | jq '. // empty')
-        if [ -z "$accountexport" ];   then
-            echo "Account export failed:" ${accountListJson}
-        else
-            
-            mkdir -p ./assets/accounts
-            cd ./assets/accounts
-            echo "$accountListJson" > user_accounts.json
-            echo "Account export Succeeded"
-        fi
-  cd ${HOME_DIR}/${repoName}
-
-
-  # Exporting Project Referencedata
-  PROJECT_ID_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}
+PROJECT_ID_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}
 
   projectJson=$(curl  --location --request GET ${PROJECT_ID_URL} \
       --header 'Content-Type: application/json' \
@@ -250,6 +153,128 @@ if [ ${synchProject} == true ]; then
           echo "Reference Data export Succeeded"
         fi
   cd ${HOME_DIR}/${repoName}
+  } 
+
+function exportAsset(){
+
+LOCAL_DEV_URL=$1
+admin_user=$2
+admin_password=$3
+repoName=$4
+assetID=$5
+assetType=$6
+HOME_DIR=$7
+
+echod ${assetType}
+
+# Single assetType
+
+if [[ $assetType = workflow* ]]; then
+    echod $assetType
+    FLOW_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}/workflows/${assetID}/export
+    cd ${HOME_DIR}/${repoName}
+    mkdir -p ./assets/workflows
+    cd ./assets/workflows
+    echod "Workflow Export:" ${FLOW_URL}
+    echod $(ls -ltr)
+else
+  if [[ $assetType = flowservice* ]]; then
+    FLOW_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}/flows/${assetID}/export
+    cd ${HOME_DIR}/${repoName}
+    mkdir -p ./assets/flowservices
+    cd ./assets/flowservices
+    echo "Flowservice Export:" ${FLOW_URL}
+    echod $(ls -ltr)
+  fi
+fi
+
+  
+    linkJson=$(curl  --location --request POST ${FLOW_URL} \
+    --header 'Content-Type: application/json' \
+    --header 'Accept: application/json' \
+    -u ${admin_user}:${admin_password})
+
+    downloadURL=$(echo "$linkJson" | jq -r '.output.download_link')
+    
+    regex='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
+    
+    if [[ $downloadURL =~ $regex ]]; then 
+       echod "Valid Download link retreived:"${downloadURL}
+    else
+        echo "Download link retreival Failed:" ${linkJson}
+        exit 1
+    fi
+    downloadJson=$(curl --location --request GET "${downloadURL}" --output ${assetID}.zip)
+
+    FILE=./${assetID}.zip
+    if [ -f "$FILE" ]; then
+        echo "Download succeeded:" ls -ltr ./${assetID}.zip
+    else
+        echo "Download failed:"${downloadJson}
+    fi
+
+# For Single assetType Flowservice Export Reference Data
+if [[ $assetType = flowservice* ]]; then
+  exprtReferenceData${LOCAL_DEV_URL} ${admin_user} ${admin_password} ${repoName} ${assetID} ${assetType} ${HOME_DIR} 
+
+fi
+
+
+cd ${HOME_DIR}/${repoName}
+
+}  
+if [ ${synchProject} == true ]; then
+  echod "Listing All Assets"
+  echod $assetType
+  PROJECT_LIST_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}/assets
+
+  projectListJson=$(curl  --location --request GET ${PROJECT_LIST_URL} \
+    --header 'Content-Type: application/json' \
+    --header 'Accept: application/json' \
+    -u ${admin_user}:${admin_password})
+  
+  # Exporing Workflows
+  for item in $(jq  -c -r '.output.workflows[]' <<< "$projectListJson"); do
+    echod "Inside Workflow Loop"
+    assetID=$item
+    assetType=workflow
+    echod $assetID
+    exportAsset ${LOCAL_DEV_URL} ${admin_user} ${admin_password} ${repoName} ${assetID} ${assetType} ${HOME_DIR}
+  done
+  # Exporting Flows
+  for item in $(jq  -c -r '.output.flows[]' <<< "$projectListJson"); do
+    echod "Inside FS Loop"
+    assetID=$item
+    assetType=flowservice
+    echod $assetID
+    exportAsset ${LOCAL_DEV_URL} ${admin_user} ${admin_password} ${repoName} ${assetID} ${assetType} ${HOME_DIR}
+  done
+
+  #Expoting Accounts
+  ACCOUNT_LIST_URL=${LOCAL_DEV_URL}/apis/v1/rest/projects/${repoName}/accounts
+
+  accountListJson=$(curl  --location --request GET ${ACCOUNT_LIST_URL} \
+      --header 'Content-Type: application/json' \
+      --header 'Accept: application/json' \
+      -u ${admin_user}:${admin_password})
+
+
+      accountexport=$(echo "$accountListJson" | jq '. // empty')
+        if [ -z "$accountexport" ];   then
+            echo "Account export failed:" ${accountListJson}
+        else
+            
+            mkdir -p ./assets/accounts
+            cd ./assets/accounts
+            echo "$accountListJson" > user_accounts.json
+            echo "Account export Succeeded"
+        fi
+  cd ${HOME_DIR}/${repoName}
+
+
+  # Exporting Project Referencedata
+
+exprtReferenceData${LOCAL_DEV_URL} ${admin_user} ${admin_password} ${repoName} ${assetID} ${assetType} ${HOME_DIR} 
 
 
   # Exporting Project Parameters
