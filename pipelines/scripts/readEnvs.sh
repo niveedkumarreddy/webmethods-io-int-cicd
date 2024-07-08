c8897b200b02db239f0a692e46e89d87d9dd9f31
srini <srinivasan.sabapathy@ibm.com>
2024-07-08 14:01:30 +0200

    updated yq path

--- pipelines/scripts/readEnvs.sh
+++ pipelines/scripts/readEnvs.sh
@@ -40,7 +40,8 @@ if [ ${#env_files[@]} -gt 0 ]; then
         base_name=${base_name%.*}
         echod $base_name${env%.*}
         echod $parent_name
-        envArr[i]=$(cat env | yq -e '.tenant.type // empty')
+        envArr[i]=$(cat $env | yq -e '.tenant.type // empty')
+        echod $envArr[i]
         let i++    
     done
     envs=$(IFS=$','; echo "${envArr[*]}")
