482a56aacb4fec91d727987e7bae0fb9f086f8c6
srini <srinivasan.sabapathy@ibm.com>
2024-07-08 13:44:58 +0200

    Adjusted paths for Environment configs

--- pipelines/scripts/readEnvs.sh
+++ pipelines/scripts/readEnvs.sh
@@ -6,15 +6,10 @@
 #                                                                            #
 #############################################################################
 
-HOME_DIR=$1
-CONFIG_DIR=$2
+
+CONFIG_DIR=$1
 debug=${@: -1}
 
-    if [ -z "$HOME_DIR" ]; then
-      echo "Missing template parameter HOME_DIR"
-      exit 1
-    fi
-    
     if [ -z "$CONFIG_DIR" ]; then
       echo "Missing template parameter CONFIG_DIR"
       exit 1
@@ -35,8 +30,8 @@ function echod(){
 declare -a envArr
 i=0
 envs=''
-if [ -e $HOME_DIR/$CONFIG_DIR/*.yml ]; then
-    for env in $HOME_DIR/$CONFIG_DIR/*.yml; do 
+if [ -e $CONFIG_DIR/*.yml ]; then
+    for env in $CONFIG_DIR/*.yml; do 
         echod $env
         base_name=${env##*/}
         parent_name="$(basename "$(dirname "$env")")"
