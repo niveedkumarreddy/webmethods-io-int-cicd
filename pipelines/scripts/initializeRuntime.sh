2c30fc1ee8d8c77964b40c5b332b4fcd93e028c4
srini <srinivasan.sabapathy@ibm.com>
2025-02-27 23:56:18 +0100

    Getting HTTP Code

--- pipelines/scripts/initializeRuntime.sh
+++ pipelines/scripts/initializeRuntime.sh
@@ -62,7 +62,7 @@ RUNTIME_REGISTER_URL=${LOCAL_DEV_URL}/apis/v1/rest/control-plane/runtimes/
   registerRuntimeJson=$(curl  --location --request POST ${RUNTIME_REGISTER_URL} \
   --header 'Content-Type: application/json' \
   --header 'Accept: application/json' \
-  --data-raw "$runtime_json" -u ${admin_user}:${admin_password} -w " %{response_code}" ) 
+  --data-raw "$runtime_json" -u ${admin_user}:${admin_password} -w "%{stderr}{\"status\": \"%{http_code}\", \"body\":\"%{stdout}\"}" ) 
 
   echo $registerRuntimeJson
   #outputArray=(${registerRuntimeJson//;/ })
