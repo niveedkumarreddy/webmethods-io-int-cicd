c97ab8428e12efe681e042a1d34f2e480c93eb76
srini <srinivasan.sabapathy@ibm.com>
2025-02-28 17:19:34 +0100

    Runtime Paring Logic

--- pipelines/scripts/initializeRuntime.sh
+++ pipelines/scripts/initializeRuntime.sh
@@ -57,37 +57,48 @@ function echod(){
 RUNTIME_REGISTER_URL=${LOCAL_DEV_URL}/apis/v1/rest/control-plane/runtimes/
  runtime_json='{ "name": "'${aliasName}'", "description": "'${description}'", "visibility": "'${visibility}'" }'
 
-
-
-
   echo "Registering Runtime"
   registerRuntimeJson=$(curl --location --request POST ${RUNTIME_REGISTER_URL} \
   --header 'Content-Type: application/json' \
   --header 'Accept: application/json' \
   --data-raw "$runtime_json" -u ${admin_user}:${admin_password} -w ";-) %{http_code}")
   
+  status=$(echo $registerRuntimeJson | awk '{split($0,a,";-)"); print a[2]}')
+  body=$(echo $registerRuntimeJson | awk '{split($0,a,";-)"); print a[1]}')
+  echod "Status:"$status  
+  echod "Body:"$body  
 
-  
-  Status=$(echo $registerRuntimeJson | awk '{split($0,a,";-)"); print a[2]}')
-  Body=$(echo $registerRuntimeJson | awk '{split($0,a,";-)"); print a[1]}')
-  echo "Status:"$Status  
-  echo "Body:"$Body  
-
-if [ ${Status} -ge 200 ] && [ ${Status} -lt 300 ]; then
-    name=$(echo "$registerRuntimeJson" | jq -r '.name')
-    agentID=$(echo "$registerRuntimeJson" | jq -r '.agentID')
+if [ ${status} -ge 200 ] && [ ${status} -lt 300 ]; then
+    name=$(echo "$body" | jq -r '.name')
+    agentID=$(echo "$body" | jq -r '.agentID')
     echo "Registered "$name" with agentID "$agentID 
 else
-    message=$(echo "$registerRuntimeJson" | jq -r '.integration.message.description')
-    echo "Failed with Status Code: "$Status "and message: "$message
+    message=$(echo "$body" | jq -r '.integration.message.description')
+    echo "Failed with Status Code: "$status "and message: "$message
     exit 1
 fi
 
+RUNTIME_PAIR_URL=${LOCAL_DEV_URL}/apis/v1/rest/control-plane/runtimes/${aliasName}/instances/new-pairing-request
 
+  echo "Pairing Runtime"
+  pairRuntimeJson=$(curl --location --request POST ${RUNTIME_PAIR_URL} \
+  --header 'Content-Type: application/json' \
+  --header 'Accept: application/json' \
+  -w ";-) %{http_code}")
 
+    status=$(echo $pairRuntimeJson | awk '{split($0,a,";-)"); print a[2]}')
+    body=$(echo $pairRuntimeJson | awk '{split($0,a,";-)"); print a[1]}')
+    echod "Status:"$status  
+    echod "Body:"$body  
 
-
-
-
-
+if [ ${status} -ge 200 ] && [ ${Statustatuss} -lt 300 ]; then
+    name=$(echo "$body" | jq -r '.agentName')
+    agentID=$(echo "$body" | jq -r '.agentId')
+    echo $body > ./${aliasName}_Paired.json
+    echo "Paired "$agentName" with agentID "$agentID 
+else
+    message=$(echo "$body" | jq -r '.integration.message.description')
+    echo "Failed with Status Code: "$status "and message: "$message
+    exit 1
+fi
 
