ea8515ea659028833416e09da9b33976bf51b706
srini <srinivasan.sabapathy@ibm.com>
2025-02-28 17:28:07 +0100

    Paring Runtime Logic

--- pipelines/scripts/initializeRuntime.sh
+++ pipelines/scripts/initializeRuntime.sh
@@ -39,9 +39,10 @@ debug=${@: -1}
       exit 1
     fi
 
- if [ "$debug" == "debug" ]; then
-    echo "......Running in Debug mode ......"
-  fi
+    if [ "$debug" == "debug" ]; then
+      echo "......Running in Debug mode ......"
+      set -x
+    fi
 
 
 function echod(){
@@ -63,18 +64,18 @@ RUNTIME_REGISTER_URL=${LOCAL_DEV_URL}/apis/v1/rest/control-plane/runtimes/
   --header 'Accept: application/json' \
   --data-raw "$runtime_json" -u ${admin_user}:${admin_password} -w ";-) %{http_code}")
   
-  status=$(echo $registerRuntimeJson | awk '{split($0,a,";-)"); print a[2]}')
-  body=$(echo $registerRuntimeJson | awk '{split($0,a,";-)"); print a[1]}')
-  echod "Status:"$status  
-  echod "Body:"$body  
+  Status=$(echo $registerRuntimeJson | awk '{split($0,a,";-)"); print a[2]}')
+  Body=$(echo $registerRuntimeJson | awk '{split($0,a,";-)"); print a[1]}')
+  echod "Status:"$Status  
+  echod "Body:"$Body  
 
-if [ ${status} -ge 200 ] && [ ${status} -lt 300 ]; then
-    name=$(echo "$body" | jq -r '.name')
-    agentID=$(echo "$body" | jq -r '.agentID')
+if [ ${Status} -ge 200 ] && [ ${Status} -lt 300 ]; then
+    name=$(echo "$Body" | jq -r '.name')
+    agentID=$(echo "$Body" | jq -r '.agentID')
     echo "Registered "$name" with agentID "$agentID 
 else
-    message=$(echo "$body" | jq -r '.integration.message.description')
-    echo "Failed with Status Code: "$status "and message: "$message
+    message=$(echo "$Body" | jq -r '.integration.message.description')
+    echo "Failed with Status Code: "$Status "and message: "$message
     exit 1
 fi
 
