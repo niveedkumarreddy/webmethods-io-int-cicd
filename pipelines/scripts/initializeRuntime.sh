1faff49c3bb52e29ad452b54f4294e2c6dc86ab5
srini <srinivasan.sabapathy@ibm.com>
2025-02-28 01:32:27 +0100

    Http response handling

--- pipelines/scripts/initializeRuntime.sh
+++ pipelines/scripts/initializeRuntime.sh
@@ -64,7 +64,7 @@ RUNTIME_REGISTER_URL=${LOCAL_DEV_URL}/apis/v1/rest/control-plane/runtimes/
   registerRuntimeJson=$(curl --location --request POST ${RUNTIME_REGISTER_URL} \
   --header 'Content-Type: application/json' \
   --header 'Accept: application/json' \
-  --data-raw "$runtime_json" -u ${admin_user}:${admin_password} -w ";-)%{http_code}")
+  --data-raw "$runtime_json" -u ${admin_user}:${admin_password} -w ";-) %{http_code}")
 
   response_array=($(echo $registerRuntimeJson |sed -e "s/;-)/ /g"))
   Status=${response_array[1]}
