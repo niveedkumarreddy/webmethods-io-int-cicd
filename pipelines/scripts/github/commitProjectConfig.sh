c6e458fc349b5917ef5ec3e2c1cbf4216d7b39ca
srini <srinivasan.sabapathy@ibm.com>
2025-07-01 20:33:14 +0200

    Project config yaml creation

--- pipelines/scripts/github/commitProjectConfig.sh
+++ pipelines/scripts/github/commitProjectConfig.sh
@@ -6,22 +6,27 @@
 #                                                                               #
 #################################################################################
 
-devUser=$1
-repoName=$2
-configName=$3
-configYaml=$4
+
+repoName=$1
+configName=$2
+configPath=$3
+configValue=$4
 HOME_DIR=$5
-featureBranchName=$6
+commitFlag=$6
+devUser=$7
+featureBranchName=$8
 debug=${@: -1}
 
 
 # Validate required inputs
-[ -z "$devUser" ] && echo "Missing template parameter devUser" >&2 && exit 1
 [ -z "$repoName" ] && echo "Missing template parameter repoName" >&2 && exit 1
 [ -z "$configName" ] && echo "Missing template parameter configName" >&2 && exit 1
-[ -z "$configYaml" ] && echo "Missing template parameter configYaml" >&2 && exit 1
+[ -z "$configPath" ] && echo "Missing template parameter configPath" >&2 && exit 1
+[ -z "$configValue" ] && echo "Missing template parameter configValue" >&2 && exit 1
 [ -z "$HOME_DIR" ] && echo "Missing template parameter HOME_DIR" >&2 && exit 1
-[ -z "$featureBranchName" ] && echo "Missing template parameter featureBranchName" >&2 && exit 1
+[ -z "$commitFlag" ] && echo "Missing template parameter commitFlag" >&2 && exit 1
+
+
    
 # Debug mode
 if [ "$debug" == "debug" ]; then
@@ -33,13 +38,28 @@ function echod() {
   echo "$@" >&2
 }
 
+# Ensure directory exists
+mkdir -p ${HOME_DIR}/${repoName}
 cd ${HOME_DIR}/${repoName}
-echo "$configYaml" > "./$configName"
+config_file="$configName".yml
+
+# Create the file if it doesn't exist
+touch "$config_file"
+
+# Update the YAML using yq
+yq -i "$configPath = \"$configValue\"" "$config_file"
+
+echod "✅ Updated $configPath with '$configValue' in $config_file"
+
+if [ ${commitFlag} == "true" ]; then
+
+    [ -z "$featureBranchName" ] && echo "Missing template parameter featureBranchName" >&2 && exit 1
+    [ -z "$devUser" ] && echo "Missing template parameter devUser" >&2 && exit 1
 
     git config user.email "noemail.com"
     git config user.name "${devUser}"
     git add .
     git commit -m "Initialize: push the project config to repository."
     git push origin HEAD:${featureBranchName}
-
+fi
 set +x
