398017221dfa1e3f11bded41c5ed034c9d6cf4b8
niveedkumarreddy <niveedkumarreddy.bollapally@ibm.com>
2025-10-29 18:07:30 +0530

    Update exportAsset.sh

--- pipelines/scripts/exportAsset.sh
+++ pipelines/scripts/exportAsset.sh
@@ -459,9 +459,8 @@ function splitAndExportAssets() {
   local assetNameList="$5"
   local assetTypeList="$6"
 
-  # Desired processing order
-  local desiredOrder=("referenceData" "rest_api" "project_parameter" "workflow" "flowservice" "dafservice" "Scheduler" "project_configuration")
-
+  # local desiredOrder=("referenceData" "rest_api" "project_parameter" "workflow" "flowservice" "dafservice" "Scheduler" "project_configuration" "project_variable" "certificate")
+ local desiredOrder=("referenceData" "rest_api" "project_parameter" "workflow" "flowservice" "dafservice" "project_configuration")
   # Normalize input: remove spaces around commas
   assetNameList=$(echo "$assetNameList" | sed 's/ *, */,/g')
   assetTypeList=$(echo "$assetTypeList" | sed 's/ *, */,/g')
